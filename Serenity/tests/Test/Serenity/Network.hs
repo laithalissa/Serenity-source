f41faf1ef8ce0a0933428380c5bbf3ebb4166ac9
Vic Smith <vic.gbsmith@gmail.com>
2012-10-26 17:24:26 +0100

    Refactored concept of server and client talking then comparing an output into a fixture.

--- Serenity/tests/Test/Serenity/Network.hs
+++ Serenity/tests/Test/Serenity/Network.hs
@@ -14,33 +14,34 @@ test_group = testGroup "Network Tests"
 	[	testCase "Test a listening process accepts a connection" test_acceptance
 	]
 
-client :: IO ()
-client = do
-	connection <- connect 9900
-	return ()
-
-server :: TVar (Maybe Connection) -> IO ()
-server tvar = do
-	connection <- listen 9900
-	atomically $ writeTVar tvar $ Just connection
-	return ()
-
 is_connected :: Connection -> Bool
 is_connected Connected {} = True
 is_connected _ = False
 
-is_connected_maybe :: Maybe Connection -> Bool
-is_connected_maybe (Just con) = is_connected con
-is_connected_maybe _ = False
+read_until_just :: TVar (Maybe a) -> IO a
+read_until_just tvar = atomically $ do 
+	maybe_output <- readTVar tvar
+	case maybe_output of
+		Nothing -> retry
+		Just output -> return output
+
+server_client_fixture server client input = do
+	output_tvar <- atomically $ newTVar input
+	forkIO $ server output_tvar
+	forkIO client
+	read_until_just output_tvar
 
 test_acceptance = do
-	serverConnection <- atomically $ newTVar (Nothing :: Maybe Connection)
-	forkIO $ server serverConnection
-	forkIO client
-	serverConn <- atomically $ do 
-		maybe_connection <- readTVar serverConnection
-		case maybe_connection of
-			Nothing -> retry
-			Just connection -> return connection
-	True @=? (is_connected serverConn)
+	connection <- server_client_fixture server client (Nothing :: Maybe Connection)
+	True @=? (is_connected connection)
+	where
+		client :: IO ()
+		client = do
+			connection <- connect 9900
+			return ()
 
+		server :: TVar (Maybe Connection) -> IO ()
+		server tvar = do
+			connection <- listen 9900
+			atomically $ writeTVar tvar $ Just connection
+			return ()

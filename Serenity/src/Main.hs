b0d1b9242419d9cb72be0eed3c50cb136cff1936
joseph <j.b.siddall@gmail.com>
2012-10-29 23:01:23 +0000

    serenity build will now only run the server/client if the correct arguments are passed, it will no longer work if no arguments are passed

--- Serenity/src/Main.hs
+++ Serenity/src/Main.hs
@@ -11,6 +11,9 @@ import System.Environment (getArgs)
 
 main = do
 	arg <- liftM (\args -> if length args > 0 then args!!0 else "") getArgs
-	do if arg == "server" 
-		then Server.main
-		else Client.main
+        do 
+          case arg of
+            "server" -> Server.main
+            "client" -> Client.main
+            "" -> putStrLn "first argument must either be 'server' or 'client'"
+            _ -> putStrLn ("unknown arg '" ++ arg ++ "', first argument must either be 'server' or 'client'")

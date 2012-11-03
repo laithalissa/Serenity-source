96bc89c78e408170491e62fd8e1bf6520bf27761
joseph <j.b.siddall@gmail.com>
2012-11-03 22:29:44 +0000

    added an impure network layer for server to use, updated server and client to use this layer

--- Serenity/src/Serenity/Game/Client/Main.hs
+++ Serenity/src/Serenity/Game/Client/Main.hs
@@ -4,8 +4,25 @@ module Serenity.Game.Client.Main (
 where
 
 
+<<<<<<< HEAD
 import Serenity.Game.Server.Network
 
 main = do
 	connection <- runConnect "localhost" 9900
 	return ()
+=======
+import Serenity.Game.Server.SimpleNetwork.Sender
+
+main = do
+  sender <- connect "localhost" 9900
+  send sender "hello world"
+  send sender "hehehehe"
+
+-- import Serenity.Network.Transport
+-- main = do
+-- 	connection <- run_connect "localhost" 9900
+--         (_, c2) <- run_transport (send "hello, i am the master client") connection
+--         run_transport (send "this is second message") c2
+-- 	return ()
+
+>>>>>>> added an impure network layer for server to use, updated server and client to use this layer

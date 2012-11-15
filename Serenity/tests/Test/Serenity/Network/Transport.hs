765bbf1df28c2d6015e9bbf1c640dcf64403cdb6
Vic Smith <vic.gbsmith@gmail.com>
2012-11-15 16:45:57 +0000

    Duplex network message sending test.

--- Serenity/tests/Test/Serenity/Network/Transport.hs
+++ Serenity/tests/Test/Serenity/Network/Transport.hs
@@ -15,6 +15,7 @@ import qualified Serenity.Network.Message as Message
 tests = testGroup "Network Tests" 
 	[	testCase "Test a listening process accepts a connection" test_acceptance
 	,	testCase "Test two processes can exchange info over a connection" test_send_receive
+	,	testCase "Test two processes can exchange info over a connection both ways" test_send_receive_duplex
 	,	testCase "Connecting when already connecting shouldn't change connection" test_connect_when_already_connected
 	]
 
@@ -27,8 +28,8 @@ test_acceptance = do
 		port = 9900
 
 test_send_receive = do
-	string <- server_client_fixture server client
-	string @?= Message.Empty
+	message <- server_client_fixture server client
+	message @?= Message.Empty
 	where
 		client = do
 			connection <- run_connect "localhost" port
@@ -37,10 +38,30 @@ test_send_receive = do
 
 		server = do
 			connection <- run_listen port
-			string <- eval_transport (receive) connection
-			return string
+			message <- eval_transport (receive) connection
+			return message
 
 		port = 9902
+
+test_send_receive_duplex = do
+	message <- server_client_fixture server client
+	message @?= Message.Empty
+	where
+		client = do
+			connection <- run_connect "localhost" port
+			run_transport (send Message.Empty) connection
+			message <- eval_transport (receive) connection
+			run_transport (send message) connection
+			return ()
+
+		server = do
+			connection <- run_listen port
+			message <- eval_transport (receive) connection
+			run_transport (send message) connection
+			message2 <- eval_transport (receive) connection
+			return message2
+
+		port = 9898
 
 test_connect_when_already_connected = do
 	connection1 <- run_connect "localhost" port

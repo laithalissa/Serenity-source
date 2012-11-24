a546e6b3854749e707bc4642803c5a86aecae94f
Vic Smith <vic.gbsmith@gmail.com>
2012-11-24 12:51:42 +0000

    Time stamps and some doc for Message.

--- Serenity/src/Serenity/Network/Message.hs
+++ Serenity/src/Serenity/Network/Message.hs
@@ -17,13 +17,17 @@ import Data.Binary
 import Data.DeriveTH
 
 type ClientId = Int
+type Time = Int
 
+-- | A message that can be sent over the network.
 data Message = 
-	  UpdateMessage Update
-	| CommandMessage Command ClientId
-	| Empty
+	  UpdateMessage Update Time            -- ^ Updates are messages containing new GameState information to be sent from the server to the clients.
+	| CommandMessage Command ClientId Time -- ^ Commands are intention notifications sent from a specific client to the server.
+	| Empty                                -- ^ An empty message (for testing purposes).
 	deriving (Show, Eq)
 
+-- | Updates are sent from server to client and are used to update the GameState information on the clients. 
+--   Any updates should be identically sent to all clients
 data Update = 
 	UpdateEntity
 	{	entity :: Entity
@@ -36,6 +40,7 @@ data Update =
 	}
 	deriving (Show, Eq)
 
+-- | Commands are sent from the clients to the server and contain order information and other notifications of intention.
 data Command = 
 	GiveOrder
 	{	entityId :: Int
@@ -43,7 +48,7 @@ data Command =
 	}
 	deriving (Show, Eq)
 
--- More Magic
+-- Derive binary instances using deep magic.
 $(derive makeBinary ''ShipOrder)
 $(derive makeBinary ''Entity)
 $(derive makeBinary ''Update)

laithalissa/Serenity-source

commit 7e43506a903805445aaa04bdf32357d246485030

Use sendMessages from Network.Message.Utility

Serenity/src/Serenity/Game/Client/Main.hs
@@ -19,7 +19,7 @@ import Serenity.Game.Shared.GameStateUpdate (manyUpdateGameState)
 import Serenity.Game.Shared.Model.GameMap (exampleGameMap)
 
 import Serenity.Network.Message (Message(..), Command)
-import Serenity.Network.Utility (TransportInterface(..), connectChannelsIO, readTChanUntilEmpty)
+import Serenity.Network.Utility
 
 main :: [String] -> IO ()
 main args = do
@@ -48,12 +48,9 @@ handleEvent :: TChan Message -> Event -> ClientState -> IO ClientState
 handleEvent outbox event clientState = do
 	-- Get a list of commands
 	newClientState <- return $ handleInput (translateEvent event) clientState
-	let commands = ClientState.commands newClientState
 
 	-- Send commands to the server
-	sendCommands outbox commands
-
-	when (not $ null commands) $ print commands
+	sendMessages outbox messages
 
 	return $ newClientState { ClientState.commands = [] }
 
@@ -63,6 +60,8 @@ handleEvent outbox event clientState = do
 
 		wx = fromIntegral $ (fst windowSize) `div` 2
 		wy = fromIntegral $ (snd windowSize) `div` 2
+
+		messages = map (\c -> CommandMessage c 0 0) (commands clientState)
 
 handleStep :: TChan Message -> Float -> ClientState -> IO ClientState
 handleStep inbox delta clientState = do
@@ -78,10 +77,3 @@ handleStep inbox delta clientState = do
 		extractUpdate (UpdateMessage update _) = update
 		isUpdate (UpdateMessage _ _) = True
 		isUpdate _ = False
-
-sendCommands :: TChan Message -> [Command] -> IO ()
-sendCommands _ [] = return ()
-sendCommands outbox (c:cs) = do
-	atomically $ writeTChan outbox (CommandMessage c 0 0)
-	sendCommands outbox cs
-

commit d3dc11d5d392492b13fe8ced97078c0513a878f2

Fix sending of client commands to server
Get commands from newClientState. Accidentally broken in 7e43506a9038.

Serenity/src/Serenity/Game/Client/Main.hs
@@ -53,6 +53,7 @@ handleEvent :: TChan Message -> Event -> ClientState -> IO ClientState
 handleEvent outbox event clientState = do
 	-- Get a list of commands
 	newClientState <- return $ handleInput (translateEvent event) clientState
+	let messages = map (\c -> CommandMessage c 0 0) (commands newClientState)
 
 	-- Send commands to the server
 	sendMessages outbox messages
@@ -66,13 +67,10 @@ handleEvent outbox event clientState = do
 		wx = fromIntegral $ (fst windowSize) `div` 2
 		wy = fromIntegral $ (snd windowSize) `div` 2
 
-		messages = map (\c -> CommandMessage c 0 0) (commands clientState)
-
 handleStep :: TChan Message -> Float -> ClientState -> IO ClientState
 handleStep inbox delta clientState = do
 	-- Receive updates from the server
 	messages <- readTChanUntilEmpty inbox
-	print messages
 	let updates = concatMap getUpdate messages
 
 	-- Apply the updates to the game state
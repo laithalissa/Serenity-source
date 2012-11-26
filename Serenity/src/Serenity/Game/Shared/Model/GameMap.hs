5ffb336de893dd44b4d3f5c69086899a01f4de8a
joseph <j.b.siddall@gmail.com>
2012-11-26 20:52:42 +0000

    bigger better example game map

--- Serenity/src/Serenity/Game/Shared/Model/GameMap.hs
+++ Serenity/src/Serenity/Game/Shared/Model/GameMap.hs
@@ -45,27 +45,60 @@ exampleGameMap = GameMap
 	{	gameMapName = "My First Map"
 	,	gameMapSize = (500, 500)
 	,	gameMapSpawnPoints=[(50, 50)]
-	,	gameMapPlanets = 
-			[	createPlanet "Jupitor" (50, 50)
-			,	createPlanet "Mars" (450, 50)
-			,	createPlanet "Pluto" (50, 450)
-			,	createPlanet "Venus" (450, 450)
-			,	createPlanet "Earth" (250, 250)
-			,	createPlanet "Moon" (250, 200)
+	-- ,	gameMapPlanets = 
+	-- 		[	createPlanet "Jupitor" (50, 50)
+	-- 		,	createPlanet "Mars" (450, 50)
+	-- 		,	createPlanet "Pluto" (50, 450)
+	-- 		,	createPlanet "Venus" (450, 450)
+	-- 		,	createPlanet "Earth" (250, 250)
+	-- 		,	createPlanet "Moon" (250, 200)
+	-- 		]
+	-- ,	gameMapSpaceLanes=
+	-- 		[	SpaceLane "Earth" "Jupitor"
+	-- 		,	SpaceLane "Earth" "Mars"
+	-- 		,	SpaceLane "Earth" "Pluto"
+	-- 		,	SpaceLane "Earth" "Venus"
+	-- 		,	SpaceLane "Earth" "Moon"
+	-- 		]
+	,	gameMapPlanets =
+			[	cp (20, 480)
+			,	cp (20, 250)
+			,	cp (20, 20)
+			
+			,	cp (250, 365)
+			,	cp (250, 135)
+
+			,	cp (480, 480)
+			,	cp (480, 250)
+			,	cp (480, 20)
 			]
 	,	gameMapSpaceLanes=
-			[	SpaceLane "Earth" "Jupitor"
-			,	SpaceLane "Earth" "Mars"
-			,	SpaceLane "Earth" "Pluto"
-			,	SpaceLane "Earth" "Venus"
-			,	SpaceLane "Earth" "Moon"
+			[	csl (20,480) (480, 480)
+			,	csl (20, 480) (480, 20)
+
+			,	csl (20, 250) (250, 365)
+			,	csl (20, 250) (250, 135)
+
+			,	csl (20, 20) (250, 135)
+
+			,	csl (250, 365) (480, 250)
+
+			,	csl (250, 135) (480, 20)
+
+			,	csl (480, 480) (480, 250)
+
+			,	csl (480, 250) (480, 20)
 			]
 	}
 	where
-		createPlanet name location = Planet
-			{	planetName=name
+		csl :: (Int, Int) -> (Int, Int) -> SpaceLane
+		csl l1 l2 = SpaceLane ("planet " ++ (show l1)) ("planet " ++ (show l2))
+	
+		cp :: (Int, Int) -> Planet
+		cp location@(x,y) = Planet
+			{	planetName="planet " ++ (show location)
 			,	planetType="planet1"
-			,	planetLocation=location
+			,	planetLocation=(fromIntegral x, fromIntegral y)
 			,	planetDirection=(0,1)
 			,	planetResources=Resources{fuel=10, antiMatter=10, metal=10}
 			}

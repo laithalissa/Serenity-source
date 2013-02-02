dbdd8585932c02e95dd14641947c04e11c663482
Vic Smith <vic.gbsmith@gmail.com>
2013-02-02 15:33:57 +0000

    Lensified View.hs.

--- Serenity/src/Serenity/Sheen/View.hs
+++ Serenity/src/Serenity/Sheen/View.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Serenity.Sheen.View
 (	View (..)
 ,	makeView
@@ -7,26 +9,28 @@ module Serenity.Sheen.View
 )
 where
 
-import Graphics.Gloss
-import Graphics.Gloss.Data.Extent
-import Graphics.Gloss.Interface.Pure.Game
+import Serenity.Sheen.UIEvent
+import Serenity.Sheen.Util
 
+import Control.Lens
 import Data.List (sortBy)
 import Data.Ord (comparing)
 import Data.Maybe (catMaybes)
 
-import Serenity.Sheen.UIEvent
-import Serenity.Sheen.Util
+import Graphics.Gloss
+import Graphics.Gloss.Data.Extent
+import Graphics.Gloss.Interface.Pure.Game
 
-data View world = View
-	{	viewID :: String                   -- ^ Unique identifier for the view
-	,	subviews :: [View world]           -- ^ List of views below this one
-	,	frame :: Extent                    -- ^ Rectangular area the view represents
-	,	zIndex :: Int                      -- ^ Stack position of the view
-	,	background :: Maybe Color          -- ^ Background colour
-	,	depict :: Maybe (world -> Picture) -- ^ Callback to draw world state within the view
-	,	eventHandler :: Maybe (UIEvent -> world -> world) -- ^ Callback to handle UIEvents
+data View a = View
+	{	_viewID           :: String                    -- ^ Unique identifier for the view
+	,	_viewSubviews     :: [View a]                  -- ^ List of views below this one
+	,	_viewFrame        :: Extent                    -- ^ Rectangular area the view represents
+	,	_viewZIndex       :: Int                       -- ^ Stack position of the view
+	,	_viewBackground   :: Maybe Color               -- ^ Background colour
+	,	_viewDepict       :: Maybe (a -> Picture)      -- ^ Callback to draw the state within the view
+	,	_viewEventHandler :: Maybe (UIEvent -> a -> a) -- ^ Callback to handle UIEvents
 	}
+makeLenses ''View
 
 -- | Create new view
 makeView ::
@@ -35,33 +39,33 @@ makeView ::
 	-> View world
 
 makeView ident (xmin, xmax, ymin, ymax) = View
-	{	viewID = ident
-	,	frame = makeExtent ymax ymin xmax xmin
-	,	subviews = []
-	,	zIndex = 0
-	,	background = Nothing
-	,	depict = Nothing
-	,	eventHandler = Nothing
+	{	_viewID = ident
+	,	_viewFrame = makeExtent ymax ymin xmax xmin
+	,	_viewSubviews = []
+	,	_viewZIndex = 0
+	,	_viewBackground = Nothing
+	,	_viewDepict = Nothing
+	,	_viewEventHandler = Nothing
 	}
 
 -- | Draw a view hierarchy
 drawView ::
-	View world -- ^ Root of the hierarchy to draw
-	-> world   -- ^ The world state to be used during drawing
+	View a -- ^ Root of the hierarchy to draw
+	-> a   -- ^ The world state to be used during drawing
 	-> Picture
-drawView view world = Translate (fromIntegral xmin) (fromIntegral ymin) $ Pictures $ bg ++ pict ++ children
+drawView view world = Translate (fromIntegral xmin) (fromIntegral ymin) $ Pictures $ background ++ pict ++ children
 	where
-	(ymax, ymin, xmax, xmin) = takeExtent $ frame view
+	(ymax, ymin, xmax, xmin) = takeExtent $ view^.viewFrame
 
-	bg = case background view of
+	background = case view^.viewBackground of
 		Just colour -> [coloredRectangle colour (fromIntegral $ xmax - xmin, fromIntegral $ ymax - ymin)]
 		Nothing -> []
 
-	pict = case depict view of
+	pict = case view^.viewDepict of
 		Just f -> [f world]
 		Nothing -> []
 
-	children = map (\v -> drawView v world) (orderViews $ subviews view)
+	children = map (\v -> drawView v world) (orderViews $ _viewSubviews view)
 
 -- | Handle a Gloss event
 -- This function gets the event handler from the deepest view in the
@@ -72,27 +76,24 @@ handleViewEvent ::
 	-> View world -- ^ View hierarchy
 	-> world      -- ^ Current world state
 	-> world
-handleViewEvent event view =
-	case eventToUIEvent event of
-		Just uiEvent -> case getEventHandler uiEvent view of
-					Just handler -> handler uiEvent
-					Nothing -> id
+handleViewEvent event view = case eventToUIEvent event of
+	Just uiEvent -> case getEventHandler uiEvent view of
+		Just handler -> handler uiEvent
 		Nothing -> id
+	Nothing -> id
 
 getEventHandler :: UIEvent -> View world -> Maybe (UIEvent -> world -> world)
 getEventHandler event@(ViewClick point _) view =
 	if eventInView then
 		case subviewHandlers of
-			[] -> eventHandler view
+			[] -> view^.viewEventHandler
 			hs -> Just (last hs)
 	else
 		Nothing
-
 	where
-	eventInView = pointInExtent (frame view) point
-	subviewHandlers = catMaybes $ map (getEventHandler $ translateUIEvent (-xmin) (-ymin) event) $ orderViews (subviews view)
-	(_, ymin, _, xmin) = takeExtent $ frame view
-
+	eventInView = pointInExtent (view^.viewFrame) point
+	subviewHandlers = catMaybes $ map (getEventHandler $ translateUIEvent (-xmin) (-ymin) event) $ orderViews $ view^.viewSubviews
+	(_, ymin, _, xmin) = takeExtent $ view^.viewFrame
 getEventHandler _ _ = Nothing
 
 -- | Change something about a specific view in a view hierarchy
@@ -102,11 +103,11 @@ changeView ::
 	-> View a             -- ^ View hierarchy containing the target view
 	-> View a
 changeView id f view
-	| viewID view == id = f view
-	| otherwise = view { subviews = map (changeView id f) (subviews view) }
+	| view^.viewID == id = f view
+	| otherwise = view { _viewSubviews = map (changeView id f) (_viewSubviews view) }
 
 orderViews :: [View world] -> [View world]
-orderViews = sortBy (comparing zIndex)
+orderViews = sortBy (comparing (^.viewZIndex))
 
 translateUIEvent :: Int -> Int -> UIEvent -> UIEvent
 translateUIEvent x y (ViewClick (clickX, clickY) button) = ViewClick newPoint button

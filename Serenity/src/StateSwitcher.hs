3d3f40a4bb3ad97143023d33ecb4562fab39c299
Vic Smith <vic.gbsmith@gmail.com>
2013-02-02 19:23:48 +0000

    Some renaming in StateSwitcher

--- Serenity/src/StateSwitcher.hs
+++ Serenity/src/StateSwitcher.hs
@@ -1,77 +1,79 @@
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module StateSwitcher
 (	module Graphics.Gloss
 ,	module Graphics.Gloss.Interface.IO.Game
-,	Switcher
+,	Application
 ,	Transition(..)
-,	StateWrapper(..)
-,	AppState(..)
-,	initialize
+,	ScreenState(..)
+,	Screen(..)
+,	initApplication
 ,	update
 ,	handleInput
 ,	render
-,	wrap
 ) where
 
+import Control.Lens
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Graphics.Gloss(Picture)
 import Graphics.Gloss.Interface.IO.Game(Event)
 
 -- | any screen needs to instance this class, when it has focus, these 3 functions will gain control
-class AppState state where	
+class Screen state where	
 	stateUpdate 
 		:: Float -- ^ seconds passed since this function was called last
 		-> state -- ^ current state of screen
-		-> Maybe (StateWrapper, Transition) -- ^ return type allows:
+		-> Maybe (ScreenState, Transition) -- ^ return type allows:
 		-- the current state to update itself by returning Just(updated-state-wrapper, Update),
 		-- a transition to another state by Just (new-state-wrapper, Push),
 		-- going back to previous state by Nothing
 	stateRender :: state -> Picture
-	stateInput :: Event -> state -> Maybe (StateWrapper, Transition)
+	stateInput :: Event -> state -> Maybe (ScreenState, Transition)
  
 data Transition = Push | Update
-data StateWrapper = forall a. (AppState a) => StateWrapper a
+data ScreenState = forall a. (Screen a) => ScreenState a
 
-instance AppState StateWrapper where
-	stateUpdate delta (StateWrapper s) = stateUpdate delta s
-	stateRender (StateWrapper s) = stateRender s
-	stateInput event (StateWrapper s) = stateInput event s
+instance Screen ScreenState where
+	stateUpdate delta (ScreenState s) = stateUpdate delta s
+	stateRender (ScreenState s) = stateRender s
+	stateInput event (ScreenState s) = stateInput event s
 
-data Switcher = Switcher 
-	{	stack :: [StateWrapper]
-	,	currentState :: StateWrapper
+type Mode = Int
+
+data Application = Application 
+	{	_applicationMode :: Mode
+	,	_applicationStates :: Map Mode ScreenState
 	}
+makeLenses ''Application
 
-wrap :: (AppState s) => s -> StateWrapper
-wrap s = StateWrapper s
+initApplication :: (Screen s) => s -> Application
+initApplication s = Application { _applicationMode = 0, _applicationStates = ScreenState s }
 
-initialize :: (AppState s) => s -> Switcher
-initialize s = Switcher{stack = [], currentState = StateWrapper s}
-
-update :: Float -> Switcher -> Switcher
+update :: Float -> Application -> Application
 update timeDelta switcher = transitionMatcher timeDelta switcher stateUpdate
 
-handleInput :: Event -> Switcher -> Switcher
+handleInput :: Event -> Application -> Application
 handleInput event switcher = transitionMatcher event switcher stateInput
 
 transitionMatcher 
 	:: a 
-	-> Switcher 
-	-> (a -> StateWrapper -> Maybe(StateWrapper, Transition))
-	-> Switcher
+	-> Application 
+	-> (a -> ScreenState -> Maybe(ScreenState, Transition))
+	-> Application
 
 transitionMatcher event switcher f =
-	case (f event sw) of
-		Nothing -> Switcher
+	case f event (currentState switcher) of
+		Nothing -> Application
 			{ 	stack = tail $ stack switcher
 			, 	currentState = head $ stack switcher 
 			} 
-		Just (sw2, Push) -> Switcher
+		Just (sw2, Push) -> Application
 			{ 	stack = (currentState switcher):(stack switcher) 
 			, 	currentState = sw2 
 			} 
 		Just (sw2, Update) -> switcher{currentState=sw2}
-	where
-		sw = currentState switcher
 
-render :: Switcher -> Picture
+render :: Application -> Picture
 render = stateRender . currentState

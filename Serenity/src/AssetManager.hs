20821f340f418cf5f77fc556a848119c4337892e
joseph <j.b.siddall@gmail.com>
2013-02-07 22:35:20 +0000

    added dependency yaml-light for yaml parsing, and added new module AssetsManager

---
+++ Serenity/src/AssetManager.hs
@@ -0,0 +1,28 @@
+
+module AssetManager where
+
+-- standard modules
+import qualified Data.Map as Map
+
+-- library modules
+import Control.Lens
+
+-- serenity modules
+import qualified Serenity.Model.Ship
+
+import Graphics.Gloss.Data.Picture
+	(	Picture(..)
+	)
+
+data Assets = Assets
+	{	_assetsImages :: Map String Picture
+	,	_assetsShipClasses :: Map String ShipClass
+	,	_assetsWeapons :: Map String Weapon
+	,	_assetsSystems :: Map String System
+	}
+	deriving (Show, Eq)
+makeLenses ''Assets
+
+
+initAssets :: IO (Either String Assets)
+initAssets

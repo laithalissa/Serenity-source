8fac8c6f1d38857de9764f524190de1017f11877
joseph <j.b.siddall@gmail.com>
2013-02-08 01:54:18 +0000

    implemented function getFileNames which returns all the valid file names for ships, systems, textures, weapons

--- Serenity/src/AssetsManager.hs
+++ Serenity/src/AssetsManager.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE Arrows #-}
 
 module AssetsManager where
 
 -- standard modules
+import Control.Applicative(liftA)
 import Data.Map(Map)
 import qualified Data.Map as Map
 
 -- library modules
+import Control.Arrow
 import Control.Lens
 import Data.Yaml.YamlLight
 import Paths_Serenity(getDataFileName)
-import System.EasyFile(getDirectoryContents)
+import System.EasyFile(getDirectoryContents, pathSeparator, takeExtensions)
 
 -- serenity modules
 import Serenity.Model.Ship
@@ -18,6 +21,13 @@ import Serenity.Model.Ship
 import Graphics.Gloss.Data.Picture
 	(	Picture(..)
 	)
+
+
+shipsDirName = ("ships", ".yml")
+systemsDirName = ("systems", ".yml")
+weaponsDirName = ("weapons", ".yml")
+texturesDirName = ("textures", ".bmp")
+
 
 data Assets = Assets
 	{	_assetsImages :: Map String Picture
@@ -31,9 +41,37 @@ makeLenses ''Assets
 
 initAssets :: FilePath -> IO (Either String Assets)
 initAssets addonsDir = do
-	files <- getDirectoryContents (addonsDir ++ "/ships")
-	return $ Left $ show $ files
+	(shipFiles, weaponFiles, systemFiles, textureFiles) <- getFileNames addonsDir
+	return $ Left $ show $ shipFiles
 
+
+type ShipFile = String
+type SystemFile = String
+type TextureFile = String
+type WeaponFile = String
+
+-- | gets all the relevent asset files from the directory passed
+getFileNames :: FilePath -> IO ([ShipFile], [WeaponFile], [SystemFile], [TextureFile])
+getFileNames dir = do
+	shipFiles <- getDirectoryFiles shipsDirName
+	weaponFiles <- getDirectoryFiles weaponsDirName
+	systemFiles <- getDirectoryFiles systemsDirName
+	textureFiles <- getDirectoryFiles texturesDirName
+	return (shipFiles, weaponFiles, systemFiles, textureFiles)
+
+		where
+		getDirectoryFiles :: (FilePath, String) -> IO [FilePath]
+		getDirectoryFiles = proc (child, extension) -> do
+			folder <- (uncurry subdir) -< (dir, child)
+			contents <- getDirectoryContents -< folder
+			returnA -< liftA (clean extension) contents
+
+		clean :: String -> [FilePath] -> [FilePath]
+		clean extension = filter ((==) extension . takeExtensions) 
+	
+	
+subdir :: FilePath -> FilePath -> FilePath
+subdir parent child = parent ++ (pathSeparator : child)
 
 	-- path <- getDataFileName "templates/ships/destroyer.yml"
 	-- return $ Left $ show $ path

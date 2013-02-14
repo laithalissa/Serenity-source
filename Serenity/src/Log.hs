fc6b36f343f2655937c508ec46e3c2162f908703
Jon Cave <jon@joncave.co.uk>
2013-02-14 14:50:26 +0000

    Remove unused Log module
    This was created without knowledge of Debug.Trace and is unnecessary

--- Serenity/src/Log.hs
+++
@@ -1,9 +0,0 @@
-
-module Log(logprint) where
-
-import System.IO.Unsafe(unsafePerformIO)
-
-logprint :: (Show m) => m -> a -> a
-logprint msg result = unsafePerformIO $ do
-	print msg
-	return result

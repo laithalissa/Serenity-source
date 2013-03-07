5bfd13804775903e4fb936c986a91fd0f9804bea
Vic Smith <vic.gbsmith@gmail.com>
2013-03-07 22:29:25 +0000

    Ecotype texture string and minimap corrections. Still no desert planet texture.

--- Serenity/src/Serenity/Game/UI/Minimap.hs
+++ Serenity/src/Serenity/Game/UI/Minimap.hs
@@ -38,9 +38,10 @@ picturePlanet sSize p = translate x y (color planetColor $ circleSolid 4) where
 	(x,y) = scaleToFit sSize (p^.planetLocation)
 	planetColor = case p^.planetEcotype of
 		Blue   -> dark $ dark green
-		Desert -> dark yellow
+		Desert -> dark orange
 		Metal  -> greyN 0.7
 		Ocean  -> blue
+		Star   -> bright yellow
 
 pictureSpaceLane :: (Double, Double) -> (Map Int Planet) -> SpaceLane -> Picture
 pictureSpaceLane sSize planetsMap (p1, p2) = color (dark $ dark $ dark $ dark green) $ lane where
